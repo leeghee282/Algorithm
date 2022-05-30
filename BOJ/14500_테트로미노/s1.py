5612027b5b9036cd9f740ff1c56987c4b58093f6
unknown <lghee282@gmail.com>
2022-05-30 22:50:22 +0900

    22/05/30

--- BOJ/14500_테트로미노/s1.py
+++ BOJ/14500_테트로미노/s1.py
@@ -1,5 +1,5 @@
 # 14500_테트로미노
-# 2022-05-04
+# 2022-05-30
 
 
 def dfs(si, sj, num, cnt):
@@ -15,30 +15,56 @@ def dfs(si, sj, num, cnt):
             ni = si + di[dir]
             nj = sj + dj[dir]
 
-            if 0 <= ni < N and 0 <= nj < M and visit1[ni][nj] == 0:
-                visit1[ni][nj] = 1
+            if 0 <= ni < N and 0 <= nj < M and visit[ni][nj] == 0:
+                visit[ni][nj] = 1
                 dfs(ni, nj, num + nums[ni][nj], cnt+1)
-                visit1[ni][nj] = 0
+                visit[ni][nj] = 0
 
 
 def block(si, sj):
     global max_num
 
+    if 0 <= si-1 < N and 0 <= si+1 < N and 0 <= sj-1 < M:
+        ans = nums[si][sj]
+        ans += nums[si-1][sj] + nums[si+1][sj] + nums[si][sj-1]
+        if ans > max_num:
+            max_num = ans
 
+    if 0 <= si-1 < N and 0 <= si+1 < N and 0 <= sj+1 < M:
+        ans = nums[si][sj]
+        ans += nums[si-1][sj] + nums[si+1][sj] + nums[si][sj+1]
+        if ans > max_num:
+            max_num = ans
+    
+    if 0 <= si-1 < N and 0 <= sj-1 < M and 0 <= sj+1 < M:
+        ans = nums[si][sj]
+        ans += nums[si-1][sj] + nums[si][sj-1] + nums[si][sj+1]
+        if ans > max_num:
+            max_num = ans
+
+    if 0 <= si+1 < N and 0 <= sj-1 < M and 0 <= sj+1 < M:
+        ans = nums[si][sj]
+        ans += nums[si+1][sj] + nums[si][sj-1] + nums[si][sj+1]
+        if ans > max_num:
+            max_num = ans
 
 
 N, M = map(int, input().split())
 nums = [list(map(int, input().split())) for _ in range(N)]
-visit1 = [[0]*M for _ in range(N)]
-visit2 = [0]*4
+visit = [[0]*M for _ in range(N)]
+
 di = [0, 0, 1, -1]
 dj = [1, -1, 0, 0]
 max_num = 0
 
 for i in range(N):
     for j in range(M):
-        visit1[i][j] = 1
+        visit[i][j] = 1
         dfs(i, j, nums[i][j], 0)
-        visit1[i][j] = 0
+        visit[i][j] = 0
+
+for i in range(N):
+    for j in range(M):
+        block(i, j)
 
 print(max_num)
